f21b4d7706a38525ceb5ebc3c6bd197bb3573853
Leo <medinayleo@gmail.com>
2020-09-16 15:08:21 -0400

    comment on code

--- src/Chocolate.js
+++ src/Chocolate.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Chocolate = () => {
-
+  //adding a comment to test ssh connection on github
   return (
     <div>
       <h1>ENJOY</h1>
